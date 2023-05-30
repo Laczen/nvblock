1fb4d119ca57066f4e92841097ca9399104131dc
Laczen JMS <laczenjms@gmail.com>
2023-05-30 19:05:41 +0200

    No extra sync needed
    Signed-off-by: Laczen JMS <laczenjms@gmail.com>

--- lib/src/nvblock.c
+++ lib/src/nvblock.c
@@ -847,10 +847,7 @@ int nvb_ioctl(struct nvb_info *info, uint8_t cmd, void *buffer)
 		*(uint32_t *)buffer = cfg->bsize;
 		break;
 	case NVB_CMD_CTRL_SYNC:
-		if (cfg->sync != NULL) {
-			return cfg->sync(cfg);
-		}
-
+		/* All is synced already */
 		return 0;
 	default:
 		return -NVB_EINVAL;
